99cc3d0f7e150aca6c0d8a3d609337a7d2521510
RobsonVess <robsonvess@gmail.com>
2018-09-09 18:26:23 -0300

    player movimentation
    First player movimentation prototype with clamp

---
+++ point_and_click_pratice/Assets/_Scripts/PlayerManager/PlayerMovimentation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerMovimentation : MonoBehaviour {
+	public float speed;
+
+	void Update () {
+		Movimentation();	
+	}
+
+	private void Movimentation() {
+		transform.Translate(CrossPlatformInputManager.GetAxis("Horizontal")*speed*Time.deltaTime,CrossPlatformInputManager.GetAxis("Vertical")*speed*Time.deltaTime,0);
+		ClampPlayerOnScreen();
+	}
+	private void ClampPlayerOnScreen() {
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.4f, 2.4f),Mathf.Clamp(transform.position.y, -4.3f, 4.3f),0);
+	}
+}
