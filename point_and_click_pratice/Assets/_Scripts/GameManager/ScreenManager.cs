25ebebb864d8d31563b0dd64f841635248744d25
RobsonVess <robsonvess@gmail.com>
2018-09-09 00:04:40 -0300

    screen setup logic

--- point_and_click_pratice/Assets/_Scripts/GameManager/ScreenManager.cs
+++ point_and_click_pratice/Assets/_Scripts/GameManager/ScreenManager.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScreenManager : MonoBehaviour {
+	private Dictionary<string ,GameObject> screens = new Dictionary<string, GameObject>();
 
-	// Use this for initialization
-	void Start () {
-		
+	public ScreenSetUp[] ScreenSetUps;
+	
+	void Awake () {
+		SetUpScreens("Main Menu");
+	}
+
+	private void SetUpScreens(string value) {
+		foreach (var screen in ScreenSetUps) {
+			screens.Add(screen.name, screen.screen);
+		}
+
+		if (screens.ContainsKey(value)) {
+			foreach (KeyValuePair<string,GameObject> screen in screens) {
+				screen.Value.SetActive(false);
+			}
+			screens[value].SetActive(true);
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+}
+[System.Serializable]
+public struct ScreenSetUp {
+	public string name;
+	public GameObject screen;
 }
