7c99b5e730b6a1b38de6e19fd261520e314a635c
Robson <robsonvess@gmail.com>
2018-09-11 11:10:44 -0300

    work in level manager

---
+++ point_and_click_pratice/Assets/_Scripts/GameManager/LevelManager/LevelLabel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class LevelLabel : MonoBehaviour
+{
+	public Image backGround;
+	public Text labelTitle, buttonTitle;
+	public Button button;
+
+	public void LabelSetup(string labelTitle = "Empty", string buttonTitle = "Go!")
+	{
+		this.labelTitle.text = labelTitle;
+		this.buttonTitle.text = buttonTitle;
+	}
+	
+}
--- point_and_click_pratice/Assets/_Scripts/GameManager/LevelManager/LevelManager.cs
+++ point_and_click_pratice/Assets/_Scripts/GameManager/LevelManager/LevelManager.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-	public GameObject button, content;
+	public GameObject levelLabel, content;
 	public static LevelManager i;
 		
 	private void Awake()
 	{
 		i = this;
 	}
-	// Make a list of panels
+	
 	public void LevelSelect(int value)
 	{
 		ScreenManager.i.ChangeScreen("Game Level");
 	}
-
+	
 	public void DebugLabel()
 	{
-	    GameObject buttons = Instantiate(button, content.transform.position, content.transform.rotation);
-		buttons.transform.SetParent(content.transform);
-		buttons.transform.localScale = new Vector3(1, 1, 1);
+	    GameObject Label = Instantiate(levelLabel);
+		Label.transform.SetParent(content.transform);
+		Label.transform.localScale = new Vector3(1, 1, 1);
 	}
+	
+}
+
+[System.Serializable]
+public struct LevelRules
+{
+	public string levelName;
+	public int levelId;
+
 }
